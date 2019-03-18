8d04cc0fc9ee144a377dfb15f218f4f646424860
ndlakhani <neildlakhani@gmail.com>
2019-03-18 05:41:57 +0000

    Add latticeEnergy function
    Calculation of energy for given lattice configuration

--- metropolis.py
+++ metropolis.py
@@ -22,5 +22,14 @@ def metropolis(lattice, beta):                                                  
             lattice[x,y] = z                                                                            # ASSIGN NEW LATTICE POINT VALUE (either 1 or -1)
     return lattice
 
-
+def latticeEnergy(lattice):                                                                             # CALCULATE ENERGY OF LATTICE CONFIGURATION
+    E1 = 0
+    for x in range(len(lattice)):                                                                       # CALCULATE OVER ENTIRE LATTICE - ALL LATTICE POINTS
+        for y in range(len(lattice)):                                                                   
+            Z = lattice[x,y]
+            Z1 = lattice[(x+1)%N, y] + lattice[x,(y+1)%N] + lattice[(x-1)%N, y] + lattice[x,(y-1)%N]      
+            E1 += -Z1*Z
+            E0 = E1/4.                                                                                  
+            energy = E0
+    return energy
 
