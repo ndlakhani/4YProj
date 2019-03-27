d949b4c3e4534e39ff64fee27462e720c2a25892
ndlakhani <neildlakhani@gmail.com>
2019-03-27 04:40:08 +0000

    Update CNN.py
    Using various examples and architecture derived from research I compiled a new Deep Convolutional NN and achieved 97.2% accuracy.

--- CNN.py
+++ CNN.py
@@ -1,20 +1,95 @@
 from __future__ import division
+import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
 from keras import regularizers
 
-N = 16                                                                      # SIZE OF SYSTEM
+# SIZE OF SYSTEM
+N = 32                                                                      
 
-Configs         = np.load("latticelist.npy")                                # LOAD LATTICE CONFIGURATIONS
-#maglabels       = np.load("maglabels.txt")                                  # LOAD MAGNETISATION LABELS
-templabels      = np.load("templist.npy")                                   # LOAD TEMPERATURE LABELS
+# LOAD LATTICE CONFIGURATIONS
+train_dataset = np.load("latticelist.npy")
 
-print("Loaded 2D Ising Lattice configurations for training")                # TRACKING OUTPUT - DECLARE SUCCESSFUL IMPORT
+# LOAD MAGNETISATION LABELS (IF UNCOMMENTED)                     
+# maglabels = np.load("maglabels.txt")
+          
+# LOAD TEMPERATURE LABELS                                          
+label = np.array(np.load("templist.npy"))
 
-test_x          = np.array(np.loadtxt("testconfigs.txt"))
-test_label      = np.array(np.loadtxt("testtemplabels.txt"))
+# TRACKING OUTPUT - DECLARE SUCCESSFUL IMPORT
+print("Loaded 2D Ising Lattice configurations for training")                
 
-print("Loaded 2D Ising Lattice configurations for testing")                 # TRACKING OUTPUT - DECLARE SUCCESSFUL IMPORT
+test_dataset = np.array(np.load("testconfigs.npy"))
+tlabels = np.array(np.load("testtemplabels.npy"))
 
+# TRACKING OUTPUT - DECLARE SUCCESSFUL IMPORT
+print("Loaded 2D Ising Lattice configurations for testing")                 
+
+
+x = train_dataset
+y = np.round(label*10)
+
+test_x = test_dataset
+test_y = np.round(tlabels*10)
+
+# PREPARING DATA: RESHAPE LATTICE TO N*N*1, COVERT LABELS TO CATEGORICAL
+
+latticeshape = (N, N, 1)
+x_train = x.reshape(x.shape[0], N, N, 1)
+x_test = test_x.reshape(test_x.shape[0],N,N,1)
+y_train = keras.utils.to_categorical(y)
+y_test = keras.utils.to_categorical(test_y)
+
+# CREATE MODEL
+model = Sequential()
+
+# CONVOLUTION LAYERS
+model.add(Conv2D(64, kernel_size=(2, 2), activation='relu', input_shape=latticeshape))
+model.add(Conv2D(32, (2, 2), activation='relu'))
+
+# MAX POOL LAYER
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# DROPOUT AND FLATTEN
+model.add(Dropout(0.2))
+model.add(Flatten())
+
+# DENSE LAYERS
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.1))
+
+# OUTPUT LOGIT LAYER
+model.add(Dense(36, activation='softmax'))
+
+# DISPLAY NETWORK ARCHITECTURE
+model.summary()
+
+# COMPILE MODEL
+model.compile(loss='binary_crossentropy',
+              optimizer="adam",
+              metrics=['accuracy'])
+
+# TRAIN MODEL
+history = model.fit(x_train, y_train,
+                    batch_size=100,
+                    epochs=20,
+                    verbose=1,
+                    shuffle=True,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+xpredict = np.array(np.load("predictdata.npy"))
+xpred = xpredict.reshape(xpredict.shape[0], N, N, 1)
+
+ypredict = model.predict_classes(xpred)
+
+truelabels = np.array(np.load("predictlabels.npy"))
+ylabels = np.round(truelabels*10)
