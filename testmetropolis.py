5976c5f658ca3fd6021c90fd376c2c2e1ea9969a
ndlakhani <neildlakhani@gmail.com>
2019-04-01 12:27:54 +0100

    Update

--- testmetropolis.py
+++ testmetropolis.py
@@ -2,28 +2,40 @@ from __future__ import division
 import numpy as np
 from numpy.random import rand
 
-def initstate(N):                                                                                       # GENERATE INITIAL SPINS
-    init = 2*np.random.randint(2, size=(N,N))-1                                                         # GENERATES ARRAY OF SIZE N BY N OF 1s and -1s
+def initstate(N):                                                                                       
+    # GENERATE INITIAL SPINS
+    init = 2*np.random.randint(2, size=(N,N))-1                                                         
+    # GENERATES ARRAY OF SIZE N BY N OF 1s and -1s
     return init
 
-def metropolis(lattice, beta):                                                                          # MONTE CARLO VIA METROPOLIS
+def metropolis(lattice, beta):                                                                          
+    # MONTE CARLO VIA METROPOLIS
     for a in range(N):
         for b in range(N):
-            x = np.random.randint(0,N)                                                                  # SELECT LATTICE POINT X-COORDINATE
-            y = np.random.randint(0,N)                                                                  # SELECT LATTICE POINT Y-COORDINATE
-            z = lattice[x, y]                                                                           # DEFINE LATTICE POINT VALUE
+            x = np.random.randint(0,N)                                                                  
+            # SELECT LATTICE POINT X-COORDINATE
+            y = np.random.randint(0,N)                                                                  
+            # SELECT LATTICE POINT Y-COORDINATE
+            z = lattice[x, y]                                                                           
+            # DEFINE LATTICE POINT VALUE
             z1 = lattice[(x+1)%N,y] + lattice[x,(y+1)%N] + lattice[(x-1)%N,y] + lattice[x,(y-1)%N]      
-            prob = 2*z*z1                                                                               # CALCULATE ENERGY CHANGE (dE)
+            prob = 2*z*z1                                                                               
+            # CALCULATE ENERGY CHANGE (dE)
             if prob < 0:                                
-                z *= -1                                                                                 # ACCEPT FLIP IF dE < 0
+                z *= -1                                                                                 
+                # ACCEPT FLIP IF dE < 0
             elif rand() < np.exp(-prob*beta):
-                z *= -1                                                                                 # ACCEPT FLIP WITH PROBABILITY OF e^(=dE/T)
-            lattice[x,y] = z                                                                            # ASSIGN NEW LATTICE POINT VALUE (either 1 or -1)
+                z *= -1                                                                                 
+                # ACCEPT FLIP WITH PROBABILITY OF e^(=dE/T)
+            lattice[x,y] = z                                                                            
+            # ASSIGN NEW LATTICE POINT VALUE (either 1 or -1)
     return lattice
 
-def latticeEnergy(lattice):                                                                             # CALCULATE ENERGY OF LATTICE CONFIGURATION
+def latticeEnergy(lattice):                                                                             
+    # CALCULATE ENERGY OF LATTICE CONFIGURATION
     E1 = 0
-    for x in range(len(lattice)):                                                                       # CALCULATE OVER ENTIRE LATTICE - ALL LATTICE POINTS
+    for x in range(len(lattice)):                                                                       
+        # CALCULATE OVER ENTIRE LATTICE - ALL LATTICE POINTS
         for y in range(len(lattice)):                                                                   
             Z = lattice[x,y]
             Z1 = lattice[(x+1)%N, y] + lattice[x,(y+1)%N] + lattice[(x-1)%N, y] + lattice[x,(y-1)%N]      
@@ -37,10 +49,14 @@ def latticeMagnetisation(lattice):
     magnetisation = np.sum(lattice)
     return magnetisation
 
-temppoints  = 100                                                                                       # NUMBER OF POINTS IN TEMPERATURE RANGE
-N           = 16                                                                                        # LATTICE LENGTH
-equilibrium = 512                                                                                      # NUMBER OF METROPOLIS RUNS TO REACH EQUILIBRIUM
-montecarlo  = 512                                                                                       # NUMBER OF METROPOLIS RUNS TO PERFORM CALCULATIONS
+temppoints  = 100                                                                                       
+# NUMBER OF POINTS IN TEMPERATURE RANGE
+N           = 16                                                                                        
+# LATTICE LENGTH
+equilibrium = 512                                                                                      
+# NUMBER OF METROPOLIS RUNS TO REACH EQUILIBRIUM
+montecarlo  = 512                                                                                       
+# NUMBER OF METROPOLIS RUNS TO PERFORM CALCULATIONS
 T           = np.linspace(1.50, 3.50, temppoints)
 E           = np.zeros(temppoints)
 M           = np.zeros(temppoints)
@@ -56,18 +72,22 @@ LatticeList = [flatlattice]
 MagList = [0]
 TempList = [0]
 
-numberofconfigs = 10                                                                                  # NUMBER OF GENERATED ARRAYS PER TEMPERATURE POINT FOR TRAINING
+numberofconfigs = 10                                                                                  
+# NUMBER OF GENERATED ARRAYS PER TEMPERATURE POINT FOR TRAINING
 
 for i in range(numberofconfigs):
-    for tpoints in range(temppoints):                                                                   # MAIN CODE BLOCK
+    for tpoints in range(temppoints):                                                                   
+        # MAIN CODE BLOCK
         E1 = M1 = E2 = M2 = 0
         lattice = initstate(N)
         beta =1.0/T[tpoints]
     
-        for x in range(equilibrium):                                                                    # EQUILIBRIATE
+        for x in range(equilibrium):                                                                    
+            # EQUILIBRIATE
             metropolis(lattice, beta)                                  
 
-        for y in range(montecarlo):                                                                     # CALCULATE
+        for y in range(montecarlo):                                                                     
+            # CALCULATE
             metropolis(lattice, beta)          
         
             Ene = latticeEnergy(lattice)                                          
@@ -78,7 +98,8 @@ for i in range(numberofconfigs):
             M2 = M2 + Mag*Mag 
             E2 = E2 + Ene*Ene
                        
-        flatlattice = np.ravel(lattice)                                                                 # SAVE LATTICE TO NUMPY ARRAY
+        flatlattice = np.ravel(lattice)                                                                 
+        # SAVE LATTICE TO NUMPY ARRAY
         LatticeList = np.concatenate((LatticeList, [flatlattice]))          
         Mg1 = abs(n1*M1)
         Mg = np.round(Mg1, 1)                                
